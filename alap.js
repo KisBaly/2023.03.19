73a5b507101345d8288624dfc119912424d835e2
Balint(Top B) <118969351+KisBaly@users.noreply.github.com>
2023-03-26 21:27:38 +0200

    Update alap.js

--- alap.js
+++ alap.js
@@ -1,63 +1,91 @@
-const dobasok = [];
-var dobott;
 
-function veletlenszamgenereal(mettol, meddig) {
-  return Math.floor(Math.random() * (meddig - mettol + 1) + mettol);
+const dobasok = new Array();
+dobasokszama = 0;
+
+function veletlenszam(mettol,meddig){
+    return Math.floor(Math.random()*(meddig-mettol+1)+mettol)
 }
 
-function megjelenit() {
-  const div = document.getElementById("kockak");
-  div.style.backgroundImage = "url(fejlec.png)";
-  div.innerHTML = "";
-  let h1 = document.createElement("h1");
-  let br = document.createElement("br");
-  h1.style.textAlign="center";
-  div.appendChild(h1);
-  div.appendChild(br);
-  h1.innerHTML = "Dobások:";
-  for (const i in dobasok) {
-    let img = document.createElement("img");
-    img.src = dobasok[i].ertek + ".png";
-    img.dataset.index = i;
-    img.style.paddingLeft="25px";
-    img.style.paddingRight="25px";
-    img.setAttribute("onclick", "kattintas(this)");
-    img.style.width = "80px";
-    img.style.height = "80px";
-    div.appendChild(img);
-    dobasok[i].img = img;
-  }
+function megjelenit(){
+    const div = document.getElementById("kockak");
+    for (const i in dobasok) {
+        let kep = document.createElement("img");
+        kep.src = dobasok[i].ertek+".png";
+        kep.dataset.index=i;
+        kep.setAttribute("onclick","kattintas(this)");
+        div.appendChild(kep);
+        dobasok[i].kep =kep;
+    }
+    console.log(dobasok);
 }
 
-function kattintas(kocka) {
-  clearInterval(dobasok[kocka.dataset.index].idozites);
+function dobas(){
+    for (let i= 0; i < 5; i++) {
+        const kocka = {
+            ertek: 0,
+            dobhate: true
+
+        }
+        dobasok.push(kocka);       
+    }
+    megjelenit();
+    // for (let index = 0; index < dobasok.length; index++) {
+    //     console.log(dobasok[index]);
+    // }
+
+    // for (const key in dobasok) {//key alapján (index)
+    //     console.log(key,dobasok[key]);
+    // }
+
+    // for (const elem of dobasok) {
+    //     console.log(elem);
+    // }
 }
-
-function dobas() {
-  for (let i = 0; i < 5; i++) {
-    const kocka = {
-      ertek: 0,
-      dobhate: true
-    };
-    dobasok.push(kocka);
-  }
-  megjelenit();
-}
-porget();
-
-function porog(kocka) {
-  kocka.ertek = veletlenszamgenereal(1, 6);
-  kocka.img.src = kocka.ertek + ".png";
-}
-
-function porget() {
-  dobasok.forEach((kocka) => {
-    kocka.idozites = setInterval(porog, 25, kocka);
-    kocka.ertek = veletlenszamgenereal(1, 6);
-  });
-  megjelenit();
-}
-
-
-
 dobas();
+
+function porog(kocka){
+    kocka.kep.src = kocka.ertek+".png";
+    kocka.ertek = veletlenszam(1,6);
+}
+
+function kattintas(kocka){
+    if(vanePorog()){
+        clearInterval(dobasok[kocka.dataset.index].idozites);
+        dobasok[kocka.dataset.index].idozites=undefined;
+    }
+    else if (dobasok[kocka.dataset.index].ertek != 0){
+        if(dobasok[kocka.dataset.index].dobhate)
+        {
+            dobasok[kocka.dataset.index].dobhate = false;
+            dobasok[kocka.dataset.index].kep.style.border = "3px solid red";
+        }
+        else
+        {
+        
+            dobasok[kocka.dataset.index].dobhate = true;
+            dobasok[kocka.dataset.index].kep.style.border = "0px solid red";
+        }
+    }
+    
+    console.log(kocka);
+}
+
+function porget (){
+    if(dobasokszama < 3 && vanePorog() == false){
+    dobasok.forEach(kocka => {
+        if(kocka.dobhate ){
+        kocka.idozites= setInterval(porog, 50,kocka);
+        }
+    });
+    dobasokszama++;
+}
+
+}
+
+function vanePorog(){
+    let i = 0;
+    while(i < dobasok.length && dobasok[i].idozites == undefined){
+        i++;
+    }
+    return (i<dobasok.length);
+}
